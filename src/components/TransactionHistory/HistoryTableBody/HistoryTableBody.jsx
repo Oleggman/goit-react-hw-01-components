4a1c4a6e018f7179d5c736e41ca9081bb21ea87b
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-10-01 17:30:12 +0300

    Add TransactionHistory prop types

--- src/components/TransactionHistory/HistoryTableBody/HistoryTableBody.jsx
+++ src/components/TransactionHistory/HistoryTableBody/HistoryTableBody.jsx
@@ -13,3 +13,13 @@ export const HistoryTableBody = ({ items }) => {
     </tbody>
   );
 }
+
+HistoryTableBody.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.exact({
+    id: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(
+      ["deposit", "withdrawal", "payment", "invoice"]).isRequired,
+    amount: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+  }))
+}
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -10,3 +10,13 @@ export const TransactionHistory = ({ items }) => {
     </table>
   );
 }
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.exact({
+    id: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(
+      ["deposit", "withdrawal", "payment", "invoice"]).isRequired,
+    amount: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+  }))
+}
