11832d4bace2d932bfd6240501bd27432ef34a78
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-10-01 16:15:20 +0300

    Add prop types for Friends list

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import { FriendListItem } from './FriendListItem/FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul>
+      {friends.map(({id, name, avatar, isOnline}) => (
+        <FriendListItem key={id} avatar={avatar} name={name} isOnline={isOnline} />
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(PropTypes.exact({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired
+  }))
+}
